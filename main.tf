ddff4965971663056ff3e3a101c49612f95830fa
Chandan Singh <chandan.k.singh@globallogic.com>
2025-10-08 12:02:21 +0530

    adding backend key

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
+data "azurerm_resource_group" "everdriven_rg" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_key_vault" "everdriven_dev" {
   name                          = "testdriven-dev"
   location                      = var.location
-  resource_group_name           = azurerm_resource_group.everdriven_rg.name
+  resource_group_name           = data.azurerm_resource_group.everdriven_rg.name
   sku_name                      = "standard"
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   public_network_access_enabled = true
 }
 
 data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "everdriven_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
 
